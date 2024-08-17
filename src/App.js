5441cd189d98ef7ce2cdce7e877dc3eb9e927626
Ayush Kumar <130389607+ayushrskiaa@users.noreply.github.com>
2024-08-17 22:50:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './components/navbar'
+import News from './components/News'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+       <Navbar/>
+       <News/>
+      </div>
+    )
+  }
+}
